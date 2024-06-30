15ce7efafcd03324eb9d86485b9b9552e7e33123
GauravsingRajput1855 <154476057+GauravsingRajput1855@users.noreply.github.com>
2024-06-30 22:55:23 +0530

    Add files via upload

---
+++ first.js/first.js
@@ -0,0 +1,21 @@
+console.log("first code");
+names="ilwagr";
+isFollow=false;
+price=99.99;
+
+const student={
+    namef:"zatu",
+    age:20,
+    cgpa:9.0,
+    isPass:true
+}
+const insta={
+    unsme:"@sk",
+    isFollow:false,
+    followint:123,
+    followers:456
+}
+console.log(typeof insta.unsme);
+console.log(typeof insta["isFollow"]);
+console.log(typeof insta.followers);
+console.log(typeof insta.followers);
